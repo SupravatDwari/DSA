a4255e44e6797dae56b6c620da942be60badfd29
Supravat Dwari <86393808+SupravatDwari@users.noreply.github.com>
2023-06-28 21:45:09 +0530

    Create Max Depth of Tree.java

---
+++ GFG/Max Depth of Tree.java
@@ -0,0 +1,8 @@
+class Solution {
+  public static int maxDepth(Node root) {
+    // code here
+    if(root==null)
+        return 0;
+    return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+  }
+}
